92671a392f3e5cbd5358ef36097d808dac74f9c9
BertovDev <bertobautista18@gmail.com>
2024-06-25 00:41:50 -0300

    Initial commit

---
+++ run.py
@@ -0,0 +1,68 @@
+import os
+from spotify_client import SpotifyClient
+import requests
+from urllib.parse import urlencode
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+import uvicorn
+
+app = FastAPI()
+
+client_id = ""
+client_secret = ""
+redirect_uri = "http://localhost:3000/callback"
+
+
+def get_access_token_debug():
+    url = "https://accounts.spotify.com/api/token"
+    headers = {
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+    data = {
+        "grant_type": "client_credentials",
+        "client_id": client_id,
+        "client_secret": client_secret,
+    }
+
+    auth_response = requests.post(url, headers=headers, data=data).json()
+    access_token = auth_response["access_token"]
+    return access_token
+
+
+def get_access_token(auth_code: str):
+    response = requests.post(
+        "https://accounts.spotify.com/api/token",
+        data={
+            "grant_type": "authorization_code",
+            "code": auth_code,
+            "redirect_uri": redirect_uri,
+        },
+        auth=(client_id, client_secret),
+    )
+    access_token = response.json()["access_token"]
+    return access_token
+
+
+@app.get("/")
+async def auth():
+    scope = ["playlist-modify-private", "playlist-modify-public"]
+    auth_url = f"https://accounts.spotify.com/authorize?response_type=code&client_id={client_id}&redirect_uri={redirect_uri}&scope={' '.join(scope)}"
+    return HTMLResponse(content=f'<a href="{auth_url}">Authorize</a>')
+
+
+@app.get("/callback")
+async def callback(code):
+    access_token = get_access_token(code)
+    spotify_client = SpotifyClient(access_token)
+    spotify_client.maybe_remove_playlist_items()
+
+
+def debug():
+    access_token = get_access_token_debug()
+    spotify_client = SpotifyClient(access_token)
+    spotify_client.maybe_remove_playlist_items()
+
+
+if __name__ == "__main__":
+    uvicorn.run(app)
+    # debug()
---
+++ spotify_client.py
@@ -0,0 +1,73 @@
+import requests
+from urllib.parse import urlparse
+import json
+
+playlist_url = ""
+
+
+class SpotifyClient:
+    def __init__(self, access_token):
+        self.access_token = access_token
+
+    def get_playlist(self):
+        response = requests.get(
+            playlist_url,
+            headers={
+                "Content-Type": "application/json",
+                                "Authorization": f"Bearer {self.access_token}"
+            }
+        )
+        response_json = response.json()
+        return response_json
+
+    def get_playlist_total_dutation(self, playlist):
+        tracks = [track for track in playlist['tracks']['items']]
+        total_time_seconds = 0
+        for track in tracks:
+            total_time_seconds += track["track"]["duration_ms"]
+        return total_time_seconds / 1000
+
+    def maybe_remove_playlist_items(self):
+        response_json = self.get_playlist()
+        total_time = self.get_playlist_total_dutation(response_json)
+        allowed_time_seconds = 10200
+
+        if (total_time > allowed_time_seconds):
+            time_diference = total_time - allowed_time_seconds
+            self.remove_track(response_json, time_diference)
+        else:
+            print("Time is already capped.")
+
+    def remove_track(self, response_json, time_diference):
+        tracks_uri = self.get_tracks_to_delete(response_json, time_diference)
+
+        params = {
+            "tracks": tracks_uri
+        }
+
+        headers = {
+            "Authorization": f"Bearer {self.access_token}",
+            "Content-Type": "application/json"
+        }
+
+        remove_response = requests.delete(
+            f"{playlist_url}/tracks", headers=headers, data=json.dumps(params, ensure_ascii=False))
+
+        print(remove_response.text)
+        print("Delete " + str(len(tracks_uri)) + " tracks")
+
+    def get_tracks_to_delete(self, response_json, time_diference):
+        tracks_uri = []
+        tracks = [track for track in response_json['tracks']['items']]
+        amount_to_delete = 0
+        track_count = 0
+        for i in range(len(tracks)):
+            if amount_to_delete < time_diference:
+                track = tracks[i]
+                amount_to_delete += track["track"]["duration_ms"] / 1000
+                track_count += 1
+            else:
+                break
+        for i in range(track_count):
+            tracks_uri.append({"uri": str(tracks[i]["track"]["uri"])})
+        return tracks_uri
